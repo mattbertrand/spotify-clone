b969cbea12b4dad566951c90b90d9264df3890b3
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-09-24 15:42:08 -0700

    created body

--- src/App.js
+++ src/App.js
@@ -36,9 +36,15 @@ function App() {
 				dispatch({
 					type: "SET_PLAYLISTS",
 					playlists: playlists,
+				});
+			});
+			spotify.getPlaylist('37i9dQZEVXcPtIrDhUpylC').then(response => {
+				dispatch({
+					type: 'SET_DISCOVER_WEEKLY',
+					discover_weekly: response,
 				})
 			})
-		}
+		};
 	}, []);
 	return (
 		<div className="app">
