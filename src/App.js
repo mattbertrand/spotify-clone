809f2d8c77760b07299f25afc79aff5fe65c66cb
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-09-24 11:46:47 -0700

    dispatch the user to the datalayer

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Login from './components/Login';
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from 'spotify-web-api-js';
 import Player from './components/Player';
+import { useStateProviderValue } from './components/StateProvider';
+import { conditionalExpression } from '@babel/types';
 
 const spotify = new SpotifyWebApi();
 
 function App() {
 
 	const [token, setToken] = useState(null)
+	const [{ user }, dispatch] = useStateProviderValue();
 
 	useEffect(() => {
 		const hash = getTokenFromUrl();
@@ -18,15 +21,19 @@ function App() {
 
 		if (_token) {
 			setToken(_token)
-			spotify.setAccessToken(_token);
 
+			spotify.setAccessToken(_token);
+			
 			spotify.getMe().then(user => {
-				console.log("🙇" , user);
+				dispatch({
+					type: 'SET_USER',
+					user
+				})
 			})
 		}
 		console.log('I HAVE A TOKEN >>> ', token)
 	}, []);
-
+	console.log('I am a ', user)
 	return (
 		<div className="app">
 			{
--- src/components/StateProvider.js
+++ src/components/StateProvider.js
@@ -6,4 +6,6 @@ export const StateProvider = ({ initialState, reducer, children }) => (
 	<StateContext.Provider value={useReducer(reducer, initialState)}>
 		{ children }
 	</StateContext.Provider>
-) 
+);
+
+export const useStateProviderValue = () => useContext(StateContext);
