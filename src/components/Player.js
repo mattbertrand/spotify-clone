331cb314f5f702e6d1a0eda46061650291bde800
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-09-24 12:01:10 -0700

    created a sidebar component

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import '../css/Player.css';
+import Sidebar from './Sidebar';
 
-function Player() {
+function Player({ spotify }) {
 	return (
 		<div className="player">
+			<div className="player_body">
+				<Sidebar />
+			</div>
 			<h1>Welcome to Spotify</h1>
 		</div>
 	)
