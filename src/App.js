1384e9667b8ba5eecd11503be4f68e73068b5419
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-09-23 20:57:33 -0700

    useEffect and useState to get the token once user clicks on log in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Login from './components/Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+
+	const [token, setToken] = useState(null)
+
+	useEffect(() => {
+		const hash = getTokenFromUrl();
+		window.location.hash = "";
+		const _token = hash.access_token;
+
+		if (_token) {
+			setToken(_token)
+		}
+		console.log('I HAVE A TOKEN >>> ', token)
+	}, []);
+
+	return (
+		<div className="app">
+			{
+				token ? (
+					<h1>I am logged in</h1>
+				) : (
+					<Login />
+				)
+			}
+		</div>
+	);
 }
 
 export default App;
