1b6cfda6078e1bf51f4409eb30c1a6232fa82630
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-09-24 12:02:46 -0700

    created a body component

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../css/Player.css';
 import Sidebar from './Sidebar';
+import Body from './Body';
 
 function Player({ spotify }) {
 	return (
 		<div className="player">
 			<div className="player_body">
 				<Sidebar />
+				<Body />
 			</div>
 			<h1>Welcome to Spotify</h1>
 		</div>
