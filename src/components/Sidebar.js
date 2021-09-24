bafcdfbefe0c2973d8217d27eba87704f2d53783
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-09-24 12:47:10 -0700

    styled the sidebarOptions

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import SidebarOption from './SidebarOption';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
 import '../css/Sidebar.css';
 
 function Sidebar() {
@@ -10,9 +13,15 @@ function Sidebar() {
 			src="https://1000logos.net/wp-content/uploads/2017/08/Spotify-symbol-768x234.jpg"
 			alt="spotify-logo"
 			/>
-			<SidebarOption title="Home" />
-			<SidebarOption title="Search" />
-			<SidebarOption title="Your Library" />
+			<SidebarOption Icon={HomeIcon} title="Home" />
+			<SidebarOption Icon={SearchIcon} title="Search" />
+			<SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+			<strong className="sidebar__title">PLAYLISTS</strong>
+			<hr />
+
+			<SidebarOption title="Hip hop"/>
+			<SidebarOption title="Rock"/>
+			<SidebarOption title="Jazz"/>
 		</div>
 	)
 }
--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -4,7 +4,9 @@ import '../css/SidebarOption.css';
 function SidebarOption({ title, Icon }) {
 	return (
 		<div className="sidebarOption">
-			<p>{title}</p>
+			{Icon && <Icon className="sidebarOption__icon" />}
+			{Icon ? <h4>{title}</h4> : <p>{title}</p>}
+			
 		</div>
 	)
 }
